cbdef4eece6697af031f04aa556cd0c885484583
Drikadev <126597972+drika-dev@users.noreply.github.com>
2023-06-07 12:42:44 -0300

    Update formatar datas.sql

--- formatar datas.sql
+++ formatar datas.sql
@@ -1,4 +1,10 @@
+------------------------------------------------------------------
+-- Formatar datas no SQL Server
+------------------------------------------------------------------ 
+-- Usando o convert
 select
+    getdate() padrao_internacional,
+    -- Ano com 2 digitos
     convert(varchar, getdate(),   0) ' 0: mmm dd aaaa hh:mm XX',
     convert(varchar, getdate(),   1) ' 1: mm/dd/aa',
     convert(varchar, getdate(),   2) ' 2: aa.mm.dd',
@@ -14,6 +20,7 @@ select
     convert(varchar, getdate(),  12) '12: aammdd',
     convert(varchar, getdate(),  13) '13: dd mmm aaaa hh:mm:ss:mi',
     convert(varchar, getdate(),  14) '14: hh:mm:ss:mi',
+    -- Ano com 4 digitos
     convert(varchar, getdate(), 100) '100 = 0',
     convert(varchar, getdate(), 101) '101: dd-mm-aaaa',
     convert(varchar, getdate(), 102) '102: aaa.mm.dd',
@@ -29,4 +36,20 @@ select
     convert(varchar, getdate(), 112) '112: aaaammdd',
     convert(varchar, getdate(), 113) '113 = 13',
     convert(varchar, getdate(), 114) '114 = 14'
-
+ 
+-- (Format: https://docs.microsoft.com/en-us/sql/t-sql/functions/format-transact-sql)
+select
+    getdate() getdate_padrao,
+    format (getdate(), 'd', 'pt-br') getdate_BR_1,
+    format (getdate(), 'd', 'en-us') getdate_US_1,
+    format (getdate(), 'D', 'pt-br') getdate_BR_2,
+    format (getdate(), 'D', 'en-us') getdate_US_2,
+    format (getdate(), 'dd-MM-yy') getdate_custom_1,
+    format (getdate(), 'dd/MM/yyyy | HH:mm:ss') getdate_custom_2 -- HH = hora 24 horas; hh = hora 12 horas
+ 
+ 
+--  SQL2012
+select
+    replace(convert(varchar, getdate(),   3), '/', '#') 'Modo 3 substituindo / por #',
+    convert(varchar, month(getdate())) + '/' + convert(varchar, year(getdate())) 'mes/ano',
+    cast(datepart(hour, getdate()) as varchar) + ' horas ' + cast(datepart(minute, getdate()) as varchar) + ' minutos' 'hh horas mm minutos'
